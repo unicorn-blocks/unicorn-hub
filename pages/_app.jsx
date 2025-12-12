c96af9d6c73ccbeabd1ba4c6741a2e1ccc4453c5
Terence <im.tywin@gmail.com>
2025-12-12 15:51:13 +0800

    feat(tracking): add Meta Pixel + GA pageview tracking

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,30 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 import { LanguageProvider } from '../context/LanguageContext';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as fbqLib from '../lib/fbq';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    fbqLib.init();
+
+    const handleRouteChange = (url) => {
+      fbqLib.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // initial pageview
+    handleRouteChange(window.location.pathname + window.location.search);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <LanguageProvider>
       <Head>
@@ -13,4 +35,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-export default MyApp; 
+export default MyApp;
